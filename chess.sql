a339e6f2be58ff11e1327b57fa9c28cb5d9e0934
Luu Nguyen <nxuluu@gmail.com>
2021-11-25 06:23:13 +0700

    add sql

---
+++ chess.sql
@@ -0,0 +1,83 @@
+-- MySQL dump 10.13  Distrib 8.0.26, for Win64 (x86_64)
+--
+-- Host: localhost    Database: chess
+-- ------------------------------------------------------
+-- Server version	8.0.26
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `history`
+--
+
+DROP TABLE IF EXISTS `history`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `history` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `usersId` int DEFAULT NULL,
+  `date` date DEFAULT NULL,
+  `oponentName` varchar(45) DEFAULT NULL,
+  `win` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_idx` (`usersId`),
+  CONSTRAINT `fk` FOREIGN KEY (`usersId`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `history`
+--
+
+LOCK TABLES `history` WRITE;
+/*!40000 ALTER TABLE `history` DISABLE KEYS */;
+INSERT INTO `history` VALUES (1,1,'2021-02-11','Long','Thắng'),(2,4,'1970-01-01','fdsf','Win'),(3,3,'1970-01-01','test','Win'),(4,3,'1970-01-01','test','Win'),(5,3,'1970-01-01','test','Win'),(6,3,'1970-01-01','test','Win'),(7,4,'1970-01-01','nam','Win'),(8,3,'1970-01-01','test','Loss'),(9,4,'1970-01-01','nam','Win'),(10,3,'1970-01-01','test','Loss'),(11,3,'1970-01-01','test','Win'),(12,4,'1970-01-01','nam','Loss'),(13,3,'1970-01-01','test','Win'),(14,4,'1970-01-01','nam','Loss'),(15,3,'1970-01-01','test','Win'),(16,4,'1970-01-01','nam','Loss'),(17,3,'1970-01-01','test','Win'),(18,4,'1970-01-01','nam','Loss'),(19,3,'1970-01-01','test','Win'),(20,4,'1970-01-01','nam','Loss'),(21,3,'1970-01-01','test','Win'),(22,4,'1970-01-01','nam','Loss'),(23,3,'1970-01-01','test','Win'),(24,4,'1970-01-01','nam','Loss'),(25,3,'1970-01-01','test','Win'),(26,4,'1970-01-01','nam','Loss'),(27,3,'1970-01-01','test','Win'),(28,4,'1970-01-01','nam','Loss'),(29,3,'1970-01-01','test','Win'),(30,4,'1970-01-01','nam','Loss');
+/*!40000 ALTER TABLE `history` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `password` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `elo` int DEFAULT '0',
+  `win` int DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'luu','12345',10,1),(2,'ha','12456',20,2),(3,'nam','12345',170,17),(4,'test','test',30,3);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-11-25  6:22:23
